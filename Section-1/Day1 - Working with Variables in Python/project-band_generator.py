f2e52241bfcf6878563893c042970798a543c4dc
sushantjadhav <sushantjadhav@live.com>
2022-01-01 23:39:45 +0530

    adding_code_day_one_completed_1st_jan_2022

---
+++ Section-1/Day1 - Working with Variables in Python/project-band_generator.py
@@ -0,0 +1,9 @@
+# 1. Create a Greeting for you program
+print("Welcome to band Generator:")
+#2. Ask the user for the city that they grew up in.
+city_name = input("Which city you grow up? \n")
+#3. Ask the user for the name of a pet.
+pet_name = input("What is your pet name \n")
+#4. Combine the name of their city and pet name show them their band name.
+print("Your band name is " + city_name + " " + pet_name)
+#5. Make sure the input cursor show on a new line.
