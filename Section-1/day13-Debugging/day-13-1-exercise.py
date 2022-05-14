9b341540f6b20bcd5656f0f3e3e69370cd787265
sushantjadhav <47061845+jadhavsushant@users.noreply.github.com>
2022-05-14 22:01:19 +0530

    Day13-Completed

---
+++ Section-1/day13-Debugging/day-13-1-exercise.py
@@ -0,0 +1,19 @@
+
+# print even or odd number
+# the challenge copied from class
+
+number = int(input("Which number do you want to check?"))
+
+if number % 2 = 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+
+## this solution
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
---
+++ Section-1/day13-Debugging/day-13-2-exercise.py
@@ -0,0 +1,29 @@
+
+#Debug Challeges
+#Code from class
+year = input("Which year do you want to check?")
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
+  
+#Resolve the code
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
